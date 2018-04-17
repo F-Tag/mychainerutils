93eab305771a0c7b89b9879f9539730cee5c7dcf
F_Tag <25476669+F-Tag@users.noreply.github.com>
2018-04-17 15:50:23 +0900

    add NPZDataset

--- dataset.py
+++ dataset.py
@@ -1,5 +1,14 @@
 
+import json
+import os
+from copy import deepcopy
+from glob import glob
+from random import shuffle
+
 import chainer
+import numpy as np
+from chainer.dataset import DatasetMixin
+
 
 def list_converter(batch, device=None, padding=None):
     """retrun lists of xp.array
@@ -19,3 +28,47 @@ def list_converter(batch, device=None, padding=None):
             device, example[i])) for example in batch])
 
     return tuple(result)
+
+
+class NPZDataset(DatasetMixin):
+
+    def __init__(self, dataset_root, dataset_dir="", param_file="datasetparam.json"):
+        if dataset_dir:
+            data_dir = os.path.join(dataset_root, dataset_dir)
+        else:
+            data_dir = dataset_root
+        paths = sorted(
+            glob(os.path.join(data_dir, '**/*.npz'), recursive=True))
+        self._paths = paths
+        with open(os.path.join(dataset_root, param_file), 'r') as f:
+            load = json.load(f)
+
+        self.params = load
+
+    def __len__(self):
+        return len(self._paths)
+
+    def get_example(self, i):
+        path = self._paths[i]
+        return dict(np.load(path))
+
+    def get_example_from_names(self, names, random=True):
+        names = deepcopy(names)
+        if random:
+            shuffle(names)
+
+        path = None
+        for name in names:
+            for p in self._paths:
+                if name == os.path.basename(p):
+                    print(p)
+                    path = p
+                    break
+
+            if path is not None:
+                break
+
+        if path is None:
+            raise FileNotFoundError
+
+        return dict(np.load(path))
