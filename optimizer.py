d5344bdd7d942f9f5a70064e1f62d1083a6f9460
F_Tag <25476669+F-Tag@users.noreply.github.com>
2018-07-06 22:38:15 +0900

    fix

--- optimizer.py
+++ optimizer.py
@@ -6,9 +6,10 @@ def apply_weightdecay_only_w(model, rate):
     use after setup optimizer
     """
 
-    for p in model.params():
-        if p.name == 'W':
-            if hasattr(p.update_rule.hyperparam, 'weight_decay_rate'):
-                p.update_rule.hyperparam.weight_decay_rate = rate
-            else:
-                p.update_rule.add_hook(WeightDecay(rate))
+    if hasattr(model, 'params') :
+        for p in model.params():
+            if p.name == 'W':
+                if hasattr(p.update_rule.hyperparam, 'weight_decay_rate'):
+                    p.update_rule.hyperparam.weight_decay_rate = rate
+                else:
+                    p.update_rule.add_hook(WeightDecay(rate))
