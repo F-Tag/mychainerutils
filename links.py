41949ac1ee524b7efd290383b49b981bf5acb37d
F_Tag <25476669+F-Tag@users.noreply.github.com>
2018-12-01 21:46:08 +0900

    del links

--- links.py
+++ links.py
@@ -13,31 +13,12 @@ import numpy as np
 from . import functions as mF
 
 
-class Convolution1D(L.Convolution2D):
-
-    def __init__(self, in_channels, out_channels, ksize=None, stride=1, pad=0, nobias=False, initialW=None, initial_bias=None, *, dilate=1, groups=1):
-        warn("use chainer.links.Convolution1D", DeprecationWarning)
-        if ksize is None:
-            out_channels, ksize, in_channels = in_channels, out_channels, None
-        super().__init__(in_channels, out_channels, (ksize, 1), (stride, 1),
-                         (pad, 0), nobias, initialW, initial_bias, dilate=(dilate, 1), groups=groups)
-
-    def __call__(self, x):
-        """
-        x is 3D ndarray（batch size  x input chanel x length)
-        """
-        x = F.reshape(x, (x.shape[0], x.shape[1], -1, 1))
-        x = super().__call__(x)
-        x = F.reshape(x, (x.shape[0], x.shape[1], -1))
-        return x
-
-
 class TSRegressor(L.Classifier):
     compute_accuracy = False
 
     def __init__(self, predictor,
                  lossfun=F.absolute_error, label_key=-1, return_key=None):
-        
+
         try:
             signature(lossfun).parameters['reduce']
         except KeyError:
@@ -154,7 +135,7 @@ class ConvBN1D(chainer.Chain):
         self.finetune = False
         super().__init__()
         with self.init_scope():
-            self.conv = Convolution1D(
+            self.conv = L.Convolution1D(
                 in_channels, out_channels, ksize, stride, pad, True, initialW, initial_bias)
             self.bn = L.BatchNormalization(out_channels)
 
@@ -162,41 +143,7 @@ class ConvBN1D(chainer.Chain):
         return self.bn(self.conv(x), finetune=self.finetune)
 
 
-class DilatedConvolution1D(L.DilatedConvolution2D):
-
-    def __init__(self, in_channels, out_channels, ksize=None, stride=1, pad=0, dilate=1, nobias=False, initialW=None, initial_bias=None):
-        warn("use chainer.links.Convolution1D", DeprecationWarning)
-        if ksize is None:
-            out_channels, ksize, in_channels = in_channels, out_channels, None
-
-        super().__init__(in_channels, out_channels, (ksize, 1),
-                         (stride, 1), (pad, 0), dilate, nobias, initialW, initial_bias)
-
-    def __call__(self, x):
-        # x is 3D ndarray（batch size  x input chanel x length)
-        x = F.reshape(x, (x.shape[0], x.shape[1], -1, 1))
-        x = super().__call__(x)
-        x = F.reshape(x, (x.shape[0], x.shape[1], -1))
-        return x
-
-
-class Deconvolution1D(L.DeconvolutionND):
-    warn("use chainer.links.Deconvolution1D", DeprecationWarning)
-
-    def __init__(self, in_channels, out_channels, ksize, stride=1, pad=0, nobias=False, outsize=None, initialW=None, initial_bias=None):
-        super().__init__(1, in_channels, out_channels, ksize,
-                         stride, pad, nobias, outsize, initialW, initial_bias)
-
-
-class Convolution3D(L.ConvolutionND):
-    warn("use chainer.links.Convolution3D", DeprecationWarning)
-
-    def __init__(self, in_channels, out_channels, ksize, stride=1, pad=0, nobias=False, initialW=None, initial_bias=None, cover_all=False):
-        super().__init__(3, in_channels, out_channels, ksize,
-                         stride, pad, nobias, initialW, initial_bias, cover_all)
-
-
-class HighWayConv1D(Convolution1D):
+class HighWayConv1D(L.Convolution1D):
     def __init__(self, in_out_channels, ksize, stride=1, pad='center', nobias=False, initialW=None, initial_bias=None, *, dilate=1, groups=1):
         if pad == 'causal':
             pad = (ksize - 1) * dilate
@@ -233,58 +180,3 @@ def build_mlp(n_out, n_units=256, layers=5, normalize=None, activation='leaky_re
         net.append(partial(F.dropout, ratio=dropout))
     net.append(L.Linear(n_out))
     return net
-
-
-class GLU1D(chainer.Chain):
-    """
-    Deep Voice 3's Gated Linear Unit
-    """
-
-    def __init__(self, in_out_channels, ksize, dilate=1, dropout=0.0, use_cond=True, causal=True, residual=True):
-        self.causal = causal
-        self.pad = (ksize - 1) * dilate
-        if not self.causal:
-            self.pad //= 2
-
-        self.dilate = dilate
-        self.ksize = ksize
-        self.dropout = dropout
-        self.residual = residual
-        super().__init__()
-        with self.init_scope():
-            self.conv = DilatedConvolution1D(
-                in_out_channels * 2, ksize, dilate=dilate, pad=self.pad)
-            if use_cond:
-                self.conv_cond = Convolution1D(in_out_channels, 1)
-
-    def __call__(self, x, cond=None):
-        length = x.shape[-1]
-        h = self.conv(F.dropout(x, self.dropout))[..., :length]
-        h, hg = F.split_axis(h, 2, axis=1)
-        if hasattr(self, 'conv_cond') and cond is not None:
-            h += mF.softsign(self.conv_cond(cond)[..., :length])
-
-        out = h * F.sigmoid(hg)
-        if self.residual:
-            out = (out + x) * np.sqrt(0.5)
-        return out
-
-    def init_que(self):
-        if not self.causal:
-            raise Exception
-        self.que = []
-
-    def fast_forward(self, x, cond=None):
-        self.que.insert(0, x)
-        x_tmp = F.concat(self.que[::-1], -1)
-        length = x.shape[-1]
-        h = self.conv(x_tmp)[..., :-self.pad][..., -1:]
-        self.que = self.que[:self.pad]
-        h, hg = F.split_axis(h, 2, axis=1)
-        if hasattr(self, 'conv_cond') and cond is not None:
-            h += mF.softsign(self.conv_cond(cond)[..., :length])
-
-        out = h * F.sigmoid(hg)
-        if self.residual:
-            out = (out + x) * np.sqrt(0.5)
-        return out
