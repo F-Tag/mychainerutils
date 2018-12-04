c84a300d900c3e7a766a94da1393a6b3b4f3621a
F_Tag <25476669+F-Tag@users.noreply.github.com>
2018-12-05 07:20:18 +0900

    remove list_concerter

--- dataset.py
+++ dataset.py
@@ -16,25 +16,6 @@ from chainer.dataset import to_device
 from chainer.dataset.convert import _concat_arrays
 
 
-def list_converter(batch, device=None, padding=None):
-    """retrun lists of xp.array
-    """
-    if len(batch) == 0:
-        raise ValueError('batch is empty')
-
-    first_elem = batch[0]
-
-    assert isinstance(first_elem, tuple)
-    result = []
-    if not isinstance(padding, tuple):
-        padding = [padding] * len(first_elem)
-
-    for i in range(len(first_elem)):
-        result.append([chainer.Variable(chainer.dataset.convert.to_device(
-            device, example[i])) for example in batch])
-
-    return tuple(result)
-
 
 class NPZDataset(DatasetMixin):
 
