a4568df4b4a0d1462ad4900ce4b66c398379913c
F_Tag <25476669+F-Tag@users.noreply.github.com>
2019-08-12 08:27:08 +0900

    add to_numpy

--- utils.py
+++ utils.py
@@ -1,13 +1,15 @@
 
-import numpy as np
 from os import environ
 from pathlib import Path
+
+import chainer
+import numpy as np
 
 
 def get_shuffled_example(length):
     origin = np.arange(length)
     shuffled = np.random.permutation(origin)
-    dup_index = np.where(origin==shuffled)[0].tolist()
+    dup_index = np.where(origin == shuffled)[0].tolist()
     dup = len((dup_index))
     if dup == 1:
         while True:
@@ -20,8 +22,9 @@ def get_shuffled_example(length):
     elif dup >= 3:
         ch_s_index = get_shuffled_example(dup)
         shuffled[dup_index] = shuffled[dup_index][ch_s_index]
-    
+
     return shuffled
+
 
 def get_datasetroot():
     if "DATASET_ROOT" in environ:
@@ -35,6 +38,7 @@ def get_datasetroot():
 
     return ret
 
+
 def get_saveroot():
     if "SAVE_ROOT" in environ:
         ret = Path(environ["SAVE_ROOT"])
@@ -46,3 +50,9 @@ def get_saveroot():
     ret.mkdir(exist_ok=True, parents=True)
 
     return ret
+
+
+def to_numpy(v):
+    v = chainer.as_variable(v)
+    v.to_cpu()
+    return v.array
