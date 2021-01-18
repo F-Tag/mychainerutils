f957b1a8a5b0e2b7713c61f72c088bb8a740f68d
F_Tag <25476669+F-Tag@users.noreply.github.com>
2021-01-18 16:54:07 +0900

    add load_modelzipfile

--- utils.py
+++ utils.py
@@ -1,10 +1,13 @@
 
+from io import BytesIO, TextIOWrapper
 from os import environ
 from pathlib import Path
+from zipfile import ZipFile
 
 import chainer
 import chainer.functions as F
 import numpy as np
+from chainer import dataset
 
 
 def get_shuffled_example(length):
@@ -78,3 +81,28 @@ def set_config(func, name, value):
         with chainer.using_config(name, value):
             return func(*args, **kwargs)
     return ret
+
+
+def load_modelzipfile(url, param_name="hparams.json", model_name="model_last", root="~/.models"):
+
+    tmp = dataset.get_dataset_root()
+    dataset.set_dataset_root(Path(root).expanduser())
+    path = dataset.cached_download(url)
+    dataset.set_dataset_root(tmp)
+
+    with ZipFile(path) as f:
+        namelist = [name for name in f.namelist() if name[-1] !=
+                    "/" if name[-1] != "/"]
+
+        param = [name for name in namelist if param_name in name]
+        assert len(param) == 1
+        param = param.pop()
+        with TextIOWrapper(f.open(param, "r"), encoding="utf-8") as pf:
+            param = pf.read()
+
+        model = [name for name in namelist if model_name in name]
+        assert len(model) == 1
+        model = model.pop()
+        model = BytesIO(f.read(model))
+
+    return param, model
